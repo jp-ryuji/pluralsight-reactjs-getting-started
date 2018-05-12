f0d729db40c0ce98406f404b85fd193a49981a9d
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-12 18:52:21 +0900

    Add working_with_data.js

---
+++ working_with_data.js
@@ -0,0 +1,15 @@
+const Card = (props) => {
+  return (
+    <div style={{ margin: '1em' }}>
+      <img src="http://placehold.it/75" />
+      <div style={{ display: 'inline-block', marginLeft: 10 }}>
+        <div style={{ fontSize: '1.25em', fontWeight: 'bold' }}>
+          Name here ...
+        </div>
+        <div>Company Name here ...</div>
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<Card />, mountNode);
