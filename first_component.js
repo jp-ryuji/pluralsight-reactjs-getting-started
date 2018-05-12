b4059ef48772102172d983e888e8455b6b33e5ae
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-12 18:02:53 +0900

    Refactor with prevState

--- first_component.js
+++ first_component.js
@@ -4,8 +4,11 @@ class Button extends React.Component {
   handleClick = () => {
     // this === component instance
 
-    this.setState({
-      counter: this.state.counter + 1
+    // setState is asynchronous, just +1 could bring a race condition.
+    this.setState((prevState) => {
+      return {
+        counter: prevState.counter + 1
+      };
     })
   };
 
