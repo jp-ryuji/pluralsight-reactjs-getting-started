e895ccfebb736d9ddb0175e5bfe85a2663355798
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-12 18:14:09 +0900

    Refactor components for reusability

--- first_component.js
+++ first_component.js
@@ -1,22 +1,36 @@
 class Button extends React.Component {
+  render() {
+    return(
+      <button onClick={this.props.onClickFunction}>
+        +1
+      </button>
+    );
+  }
+}
+
+const Result = props => {
+  return (
+    <div>{props.counter}</div>
+  );
+};
+
+class App extends React.Component {
   state = { counter: 0 };
 
-  handleClick = () => {
-    // this === component instance
-
-    // setState is asynchronous, just +1 could bring a race condition.
+  incrementCounter = () => {
     this.setState((prevState) => ({
       counter: prevState.counter + 1
     }));
   };
 
   render() {
-    return(
-      <button onClick={this.handleClick}>
-        {this.state.counter}
-      </button>
+    return (
+      <div>
+        <Button onClickFunction={this.incrementCounter} />
+        <Result counter={this.state.counter} />
+      </div>
     );
   }
 }
 
-ReactDOM.render(<Button />, mountNode);
+ReactDOM.render(<App />, mountNode);
