6bb96cd3adb8fdb30281b4b7a8cb207ded26a483
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-12 19:23:07 +0900

    Use props for reusability

--- working_with_data.js
+++ working_with_data.js
@@ -1,12 +1,12 @@
 const Card = props => {
   return (
     <div style={{ margin: '1em' }}>
-      <img width="75" src="https://avatars3.githubusercontent.com/u/1908717?v=4" />
+      <img width="75" src={props.avatar_url} />
       <div style={{ display: 'inline-block', marginLeft: 10 }}>
         <div style={{ fontSize: '1.25em', fontWeight: 'bold' }}>
-          Name here ...
+          {props.name}
         </div>
-        <div>Company Name here ...</div>
+        <div>{props.company}</div>
       </div>
     </div>
   );
@@ -15,7 +15,11 @@ const Card = props => {
 const CardList = props => {
   return (
     <div>
-      <Card />
+      <Card
+        name='Lugi'
+        avatar_url="https://avatars3.githubusercontent.com/u/1908717?v=4"
+        company="Foo Company"
+      />
     </div>
   );
 };
