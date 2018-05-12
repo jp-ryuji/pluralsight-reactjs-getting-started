bed3721bff4304fcf342737f276ca396ae5fb722
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-12 18:00:08 +0900

    Add first_component.js

---
+++ first_component.js
@@ -0,0 +1,24 @@
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+  handleClick = () => {
+    // this === component instance
+
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  };
+
+  render() {
+    return(
+      <button onClick={this.handleClick}>
+        {this.state.counter}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(<Button />, mountNode);
