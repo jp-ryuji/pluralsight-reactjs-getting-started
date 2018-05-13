2e3bd2357e26dc9950e5c015a6ae3316172f68bd
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-13 09:27:33 +0900

    Add Stars, Button and Answer function components

--- play_nine_game.js
+++ play_nine_game.js
@@ -1,8 +1,35 @@
+const Stars = props => {
+  return (
+    <div>
+      ...
+    </div>
+  )
+};
+
+const Button = props => {
+  return (
+    <div>
+      ...
+    </div>
+  )
+};
+
+const Answer = props => {
+  return (
+    <div>
+      ...
+    </div>
+  )
+};
+
 class Game extends React.Component {
   render() {
     return (
       <div>
         <h3>Play Nine</h3>
+        <Stars />
+        <Button />
+        <Answer />
       </div>
     );
   }
