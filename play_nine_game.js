61ba4b67d8b09d1e4404a90b621d9cc620273c47
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-13 11:06:44 +0900

    Just clean up commented out code

--- play_nine_game.js
+++ play_nine_game.js
@@ -1,10 +1,5 @@
 const Stars = props => {
   const numberOfStars = 1 + Math.floor(Math.random() * 9);
-  // let stars = [];
-  // for (let i = 0; i < numberOfStars; i++) {
-  //   stars.push(<i key={i} className="fa fa-star"></i>)
-  // }
-
   return (
     <div className="col-5">
       {_.range(numberOfStars).map(i =>
