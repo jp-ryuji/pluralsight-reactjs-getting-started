3e701c85f23719a8dc31372db8e0e70b94f6489c
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-13 14:01:29 +0900

    Set answerIsCorrect on selectNumber and unselectNumber

--- play_nine_game.js
+++ play_nine_game.js
@@ -88,12 +88,14 @@ class Game extends React.Component {
   selectNumber = (clickedNumber) => {
     if (this.state.selectedNumbers.indexOf(clickedNumber) >= 0) { return; }
     this.setState(prevState => ({
+      answerIsCorrect: null,
       selectedNumbers: prevState.selectedNumbers.concat(clickedNumber)
     }));
   };
 
   unselectNumber = (clickedNumber) => {
     this.setState(prevState => ({
+      answerIsCorrect: null,
       selectedNumbers: prevState.selectedNumbers.filter(number => number !== clickedNumber)
     }));
   };
